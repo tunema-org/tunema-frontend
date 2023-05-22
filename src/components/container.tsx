ffcf22d8510de033de2789cb1ee0ae49b7bf2fd4
HotPotatoC <juandotulung@gmail.com>
2023-05-22 17:18:02 +0700

    update container

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -1,28 +1,8 @@
-import styled from 'styled-components'
+type ContainerProps = {
+  children: React.ReactNode
+}
 
-export const Container = styled.div`
-  width: 100%;
-  margin: 0 auto;
-
-  @media (min-width: 640px) {
-    max-width: 640px;
-  }
-
-  @media (min-width: 768px) {
-    max-width: 768px;
-  }
-
-  @media (min-width: 1024px) {
-    max-width: 1024px;
-  }
-
-  @media (min-width: 1280px) {
-    max-width: 1280px;
-  }
-
-  @media (min-width: 1536px) {
-    max-width: 1536px;
-  }
-`
-
+export const Container = ({ children }: ContainerProps) => (
+  <div className="container mx-auto">{children}</div>
+)
 export default Container
