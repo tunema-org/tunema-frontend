58f277db52d1446a7cf3fb67d7cc6ef5cf3756e8
Typhoon <aphen007@gmail.com>
2023-06-03 19:28:30 +0700

    sample-cart and some new files

---
+++ src/components/samples/buy-sample.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { useState } from 'react'
+import Dropdown from '../dropdown/dropdown-sample'
+import IconHeart from '../icons/icon-heart'
+import IconPlay from '../icons/icon-play'
+import IconPlus from '../icons/icon-plus'
+import IconDelete from '../icons/icon-delete'
+import Waveform from '../waveform'
+
+type SampleProps = {
+  src: string
+}
+
+const BuySample = () => {
+  return (
+    <section className="flex flex-col">
+      <div className="flex items-start border-b-[1px] border-gray-3"></div>
+      <div className="grid md:grid-cols-6 grid-cols-5 items-center gap-10 pr-2 font-medium py-5 text-gray-4 px-4">
+        <div className="">
+          <IconDelete className="hover:visible invisible" />
+        </div>
+        <div className="flex flex-col">
+          <img
+            className="w-[4rem] min-w-[3rem]"
+            src="/pic/components/player/alblumcover.png"
+            alt=""
+          />
+        </div>
+        <div className="flex flex-col md:col-span-2 col-auto min-w-[100px]">
+          <a className="font-medium hover:underline overflow-auto no-scrollbar cursor-pointer">
+            Gh0st_wave_Surround_Sound_125BPM_Am.wav
+          </a>
+        </div>
+        <p className="text-left sm:visible invisible">3:45</p>
+        <button className="flex flex-col font-medium text-dark-green hover:underline justify-start items-start">
+          $3.45
+        </button>
+      </div>
+      <div className="flex items-start border-b-[1px] border-gray-3"></div>
+    </section>
+  )
+}
+
+export default BuySample
--- src/pages/sample-cart/sample-cart.tsx
+++ src/pages/sample-cart/sample-cart.tsx
@@ -8,6 +8,7 @@ import Navbar from '../../components/Navbar/navbar'
 import Sample from '../../components/samples/sample'
 import SearchBar from '../../components/searchbar'
 import Type from '../../components/type'
+import BuySample from '../../components/samples/buy-sample'
 
 function SampleCart() {
   return (
@@ -21,6 +22,46 @@ function SampleCart() {
             <h1 className="text-heading-01 ">Sample Cart</h1>
             <p>Shopping cart.</p>
           </div>
+        </section>
+        <section className="flex md:justify-between justify-normal gap-5 font-body xl:flex-row flex-col my-5 px-0 sm:px-51  ">
+          <section className="flex flex-col">
+            <div className="flex items-start border-b-[1px] border-gray-3"></div>
+            <div className="grid md:grid-cols-6 grid-cols-5 items-center gap-10 pr-2 font-medium py-5 text-gray-4 px-4">
+              <div className="col-auto"></div>
+              <p>Cover</p>
+              <div className="md:col-span-2 col-auto min-w-[100px]">
+                <p>FileName</p>
+              </div>
+              <p className="sm:visible invisible">Time</p>
+              <p className="flex flex-col font-medium justify-start">Price</p>
+            </div>
+            <div className="flex items-start border-b-[1px] border-gray-3"></div>
+            <section>
+              <BuySample />
+            </section>
+          </section>
+
+          <section className="sm:shadow-[0px_4px_30px_rgba(0,0,0,0.08)] shadow-none rounded-[10px] w-[320px] sm:w-[430px] flex flex-col sm:p-8 p-0 items-center gap-5 font-body">
+            <div className="border-b-[1px] border-gray-3 w-full my-2 sm:hidden block"></div>
+            <div className="grid-cols-2 items-center min-w-auto grid w-full">
+              <p className="flex justify-start">Total Product</p>
+              <p className="flex justify-end text-dark-green font-medium">
+                $3.45
+              </p>
+            </div>
+            <div className="border-b-[1px] border-gray-3 w-[98%]"></div>
+            <div className="grid-cols-2 items-center min-w-auto grid w-full">
+              <p className="flex justify-start text-heading-05">
+                Total Payment
+              </p>
+              <p className="flex justify-end text-dark-green text-heading-05">
+                $3.45
+              </p>
+            </div>
+            <button className="duration-200 bg-primary-green hover:bg-dark-green w-full font-medium py-2 px-8 rounded-full overflow-clip">
+              Checkout
+            </button>
+          </section>
         </section>
       </Container>
     </>
