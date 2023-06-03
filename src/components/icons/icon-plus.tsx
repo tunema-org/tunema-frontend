986dff4714c115a9d21a76fb290d10501b50e140
Typhoon <aphen007@gmail.com>
2023-06-04 01:40:51 +0700

    icons hover and sample cart finishing

--- src/components/icons/icon-plus.tsx
+++ src/components/icons/icon-plus.tsx
@@ -1,17 +1,47 @@
-import * as React from 'react'
-const IconPlus = (props: any) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width={22}
-    height={22}
-    fill="none"
-    {...props}
-  >
-    <path
-      fill="#000"
-      fillOpacity={0.5}
-      d="M22 11a.917.917 0 0 1-.917.917h-9.166v9.166a.917.917 0 0 1-1.834 0v-9.166H.917a.917.917 0 0 1 0-1.834h9.166V.917a.917.917 0 0 1 1.834 0v9.166h9.166A.917.917 0 0 1 22 11Z"
-    />
-  </svg>
-)
+import React, { useState } from 'react'
+
+const IconPlus: React.FC = () => {
+  const [isHovered, setIsHovered] = useState(false)
+
+  const handleMouseEnter = () => {
+    setIsHovered(true)
+  }
+
+  const handleMouseLeave = () => {
+    setIsHovered(false)
+  }
+
+  return (
+    <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+      {isHovered ? (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width={22}
+          height={22}
+          fill="#9CB719"
+        >
+          <path
+            fill="#9CB719"
+            fillOpacity={1}
+            d="M22 11a.917.917 0 0 1-.917.917h-9.166v9.166a.917.917 0 0 1-1.834 0v-9.166H.917a.917.917 0 0 1 0-1.834h9.166V.917a.917.917 0 0 1 1.834 0v9.166h9.166A.917.917 0 0 1 22 11Z"
+          />
+        </svg>
+      ) : (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width={22}
+          height={22}
+          fill="none"
+        >
+          <path
+            fill="#000"
+            fillOpacity={0.5}
+            d="M22 11a.917.917 0 0 1-.917.917h-9.166v9.166a.917.917 0 0 1-1.834 0v-9.166H.917a.917.917 0 0 1 0-1.834h9.166V.917a.917.917 0 0 1 1.834 0v9.166h9.166A.917.917 0 0 1 22 11Z"
+          />
+        </svg>
+      )}
+    </div>
+  )
+}
+
 export default IconPlus
