146d5f3adb7f5df1c1893edf77cd8231ddd3f326
Typhoon <aphen007@gmail.com>
2023-06-06 14:04:44 +0700

    type update

--- src/components/type.tsx
+++ src/components/type.tsx
@@ -1,23 +1,54 @@
 import React from 'react'
 import { useState, useRef } from 'react'
 
-interface props {
+type TypeText = {
   children: string
 }
 
-const Type = ({ children }: props) => {
-  const typeVariant = {
-    primary: 'border-black bg-none',
-    clicked: 'border-dark-green bg-light-green',
+const Type = (props: TypeText) => {
+  const [isClicked, setIsClicked] = useState(false)
+
+  const handleClick = () => {
+    setIsClicked(!isClicked)
   }
 
   return (
     <>
-      <button className="border border-black bg-none rounded-[10px] w-auto py-1 px-3 hover:bg-light-green hover:border-dark-green">
-        {children}
+      <button
+        className={`border border-black bg-none rounded-[10px] w-auto py-1 px-3 hover:bg-light-green hover:border-dark-green ${
+          isClicked
+            ? 'transition ease-out duration-150 border-dark-green bg-light-green'
+            : 'transition ease-out duration-150 border-black bg-none'
+        }`}
+        onClick={handleClick}
+      >
+        {props.children}
       </button>
     </>
   )
 }
 
 export default Type
+
+// import React, { useState } from 'react';
+
+// const ToggleButton = () => {
+//   const [isClicked, setIsClicked] = useState(false);
+
+//   const handleClick = () => {
+//     setIsClicked(!isClicked);
+//   };
+
+//   return (
+//     <button
+//       className={`px-4 py-2 rounded ${
+//         isClicked ? 'bg-red-500 text-white' : 'bg-blue-500 text-black'
+//       }`}
+//       onClick={handleClick}
+//     >
+//       Click Me
+//     </button>
+//   );
+// };
+
+// export default ToggleButton;
