0dd8848f7102476bb5acc25a289a26ade2a46d17
MarHansen <hansenmartino100@gmail.com>
2023-06-20 13:10:56 +0700

    added sidebar auth

--- src/components/Navbar/sidebar.tsx
+++ src/components/Navbar/sidebar.tsx
@@ -1,11 +1,15 @@
 import { useState, useEffect, useRef } from 'react'
 import React from 'react'
 import { useDraggable } from 'react-use-draggable-scroll'
+import { useAuthStore } from '../../store/useAuthStore'
+import { Link, useNavigate } from 'react-router-dom'
 
 const Sidebar = () => {
   const ref =
     useRef<HTMLDivElement>() as React.MutableRefObject<HTMLInputElement>
   const { events } = useDraggable(ref)
+  const auth = useAuthStore()
+  const navigate = useNavigate()
 
   const hoverVariant = {
     left: 'w-max duration-200 relative mt-auto mb-auto hover:text-dark-green hover:before:scale-x-100 before:absolute before:origin-top-left before:block before:transition-transform before:duration-[0.3s] before:ease-[ease] before:scale-x-0 before:left-0 before:bottom-0 before:w-full before:h-0.5 before:bg-[#9CB719]',
@@ -22,16 +26,18 @@ const Sidebar = () => {
       >
         <div className="flex flex-col gap-7">
           {' '}
-          <a href="/artist-dashboard">
-            <button className="cursor-pointer flex items-center gap-5 font-bold text-heading-04">
-              <img
-                className="rounded-full w-11"
-                src="/pic/navbar/dummy_profile.jpg"
-                alt=""
-              />
-              <h1>User</h1>
-            </button>
-          </a>
+          {auth.isAuthenticated && (
+            <a href="/artist-dashboard">
+              <button className="cursor-pointer flex items-center gap-5 font-bold text-heading-04">
+                <img
+                  className="rounded-full w-11"
+                  src={auth.user.profileImageUrl}
+                  alt=""
+                />
+                <h1>{auth.user.username}</h1>
+              </button>
+            </a>
+          )}
           <div className="flex flex-col w-auto gap-3">
             <h3 className="font-medium text-[#ababa9]">Navigation</h3>
             <a href="/home" className={hoverVariant.left}>
@@ -56,9 +62,22 @@ const Sidebar = () => {
           <a href="/" className={hoverVariant.left}>
             About TUNEMA
           </a>
-          <a href="" className="hover:underline text-red-400 font-medium">
-            Log Out
-          </a>
+          {auth.isAuthenticated && (
+            <button
+              onClick={() => {
+                auth.logout()
+                return navigate('/login')
+              }}
+              className="hover:underline flex justify-start text-red-400 font-medium"
+            >
+              Log Out
+            </button>
+          )}
+          {!auth.isAuthenticated && (
+            <a href="/login" className={hoverVariant.left}>
+              Log In
+            </a>
+          )}
         </div>
       </div>
     </>
